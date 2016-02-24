fe3969fce828e9442bbedd5505a8f52b30684ce5
Kyle Carruthers <kyle.carruthers15@gmail.com>
2016-02-24 16:08:42 -0500

    Initial commit with starting code. Ship and background are in. Ship moves.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+    
+    public float speed;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		rb.velocity = (new Vector3(moveHorizontal, 0.0f, moveVertical)) * speed;
+        
+        
+      //  rb.position = new Vector3(x, 0, z);  
+	}
+}
